9334e1600297cb3a6098417d1faff9d8f1786c83
rhparker <rosshparker@gmail.com>
2021-02-14 11:46:44 -0600

    initial commit

---
+++ clusters.m
@@ -0,0 +1,126 @@
+f = 0.8;
+
+% % no clusters
+% a = 4;
+% mee = 0.1;
+% mei = mee;
+% mie = -a*mee;
+% mii = -a*mee;
+
+% clusters 
+Nc = 1;
+mii = -1;
+mie = -1;
+mei = -((1-f)/f)*mii;
+mee = -Nc*((1-f)/f)*mii;
+
+% specify N
+N  = 20;
+p  = floor( N*f/Nc );
+ne = Nc*p;
+ni = N-ne;
+H = cluster(N, Nc, p, mee, mei, mie, mii); 
+Jtilde = [ (p-1)*mee ni*mei ; ne*mie (ni-1)*mii ];
+
+% % specify Nc, p, Nci
+% Nc = 2;
+% p = 10;
+% Nci = 3;
+% pi = round( (Nc*p/Nci)*(1-f)/f );
+% ne = Nc*p;
+% ni = Nci*pi;
+% N = ne+ni;
+% H = clusterEI(Nc, p, Nci, pi, mee, mei, mie, mii);    
+    
+l  = eig(H);
+realvals = uniquetol(real(l));
+
+gstar = sqrt(N)/realvals(end);
+g = gstar + 0.5
+g = 5
+
+l3 = (g/sqrt(N))*l-1;
+
+%%
+% timestepping
+
+x0 = randn(N,1)*0.1;
+
+fn = @(t, x)(-x + (H/sqrt(N))*tanh(g*x));
+
+t = linspace(0,100,3000);
+x = rk4(fn, x0, t);
+
+J = Hx( H, g, x(:,end) );
+lJ = eig(J);
+realvalsJ = uniquetol(real(lJ));
+xinh = abs( x(end,end) );
+k = 1 - (gstar*xinh)^2;
+
+figure;
+subplot(2,2,1);
+plot(real(l), imag(l), '.r', 'MarkerSize', 30);
+subplot(2,2,2);
+plot(real(l3), imag(l), '.', 'MarkerSize', 30);
+subplot(2,2,3);
+plot(t, x);
+subplot(2,2,4);
+l4 = g*lJ/sqrt(N) - 1;
+plot(real(l4),imag(l4), '.', 'MarkerSize', 30);
+
+
+
+%% functions
+
+% generate matrix H for linearization about origin
+function H = cluster(N, Nc, p, mee, mei, mie, mii)
+    if Nc*p >= N
+        H = 0
+    else
+        ni = N - Nc*p;
+        eblock = mee*clusterblock(Nc, p);        
+        iblock = mii*clusterblock(1, ni);     
+        eiblock = mei*ones( Nc*p, ni);
+        ieblock = mie*ones( ni, Nc*p);  
+        H = [ eblock eiblock ; ieblock iblock ];
+    end
+end
+
+% allow excitatory and inhibitory clusters
+function H = clusterEI(Nc, p, Nci, pi, mee, mei, mie, mii)
+    ni = Nci*pi;
+    ne = Nc*p;
+    eblock = mee*clusterblock(Nc, p);        
+    iblock = mii*clusterblock(Nci, pi);     
+    eiblock = mei*ones( ne, ni);
+    ieblock = mie*ones( ni, ne);  
+    H = [ eblock eiblock ; ieblock iblock ];
+end
+
+% make matrix for n blocks of size p, will be np x np
+function M = clusterblock(n, p)
+    mask  = ones(p,p) - eye(p);
+    block = ones(p,p) .* mask;
+    bcell = repmat({block}, 1, n);
+    M = blkdiag(bcell{:});
+end
+
+% matrix H(x) for arbitrary fixed point linearization
+function M = Hx(H, g, x)
+    row = sech(g*x').^2;
+    M = H .* repmat( row, length(x), 1 );
+end
+
+% Runge-Kutta 4 ODE solver
+% t is time grid
+function u = rk4(f, u0, t)
+    u = u0;
+    h = t(2) - t(1);
+    for index = 1:(length(t) - 1)
+       k1 = h*f( t(index), u(:,end) );
+       k2 = h*f( t(index)+h/2, u(:,end)+0.5*k1 );
+       k3 = h*f( t(index)+h/2, u(:,end)+0.5*k2 ); 
+       k4 = h*f( t(index)+h, u(:,end)+k3 );
+       u = [ u  u(:,end)+(k1 + 2*k2 + 2*k3 + k4)/6 ];
+    end
+end
---
+++ singlecluster.f90
@@ -0,0 +1,157 @@
+
+SUBROUTINE FUNC(NDIM,U,ICP,PAR,IJAC,F,DFDU,DFDP)
+!     ---------- ----
+
+! Evaluates the algebraic equations or ODE right hand side
+
+! Input arguments :
+!      NDIM   :   Dimension of the ODE system 
+!      U      :   State variables
+!      ICP    :   Array indicating the free parameter(s)
+!      PAR    :   Equation parameters
+
+! Values to be returned :
+!      F      :   ODE right hand side values
+
+! Normally unused Jacobian arguments : IJAC, DFDU, DFDP (see manual)
+
+      IMPLICIT NONE
+      INTEGER NDIM, IJAC, ICP(*)
+      INTEGER I, J, K
+      INTEGER NE, NI
+      DOUBLE PRECISION U(NDIM), PAR(*), F(NDIM), DFDU(*), DFDP(*)
+      DOUBLE PRECISION G, Mee, Mei, Mie, Mii
+      DOUBLE PRECISION S
+      ! REAL, dimension(NDIM, NDIM) :: H
+
+      G   = PAR(1)
+      Mee = PAR(2)
+      Mei = PAR(3)
+      Mie = PAR(4)
+      Mii = PAR(5)
+
+      NE = NDIM * 0.8
+      NI = NDIM - NE
+
+      ! ! make matrix (eventually read from file?)
+      ! DO I = 1,NE
+      !       DO J = 1,NE
+      !             H(I,J) = Mee
+      !       END DO
+      !       DO J = NE+1,NDIM
+      !             H(I,J) = Mei
+      !       END DO
+      !       H(I,I) = 0
+      ! END DO
+      ! DO I = NE+1,NDIM
+      !       DO J = 1,NE
+      !             H(I,J) = Mie
+      !       END DO
+      !       DO J = NE+1,NDIM
+      !             H(I,J) = Mii
+      !       END DO
+      !       H(I,I) = 0
+      ! END DO
+
+      DO I=1,NE
+            S = 0
+            DO J = 1,NE
+                  IF (J /= I) THEN
+                        S = S + Mee*TANH( G * U(J) )
+                        ! PRINT *,J,I
+                  END IF
+            END DO
+            DO J = NE+1,NDIM
+                  S = S + Mei*TANH( G * U(J) )
+            END DO
+            S = S / SQRT(REAL(NDIM))
+            F(I) = -U(I) + S 
+      END DO
+
+      DO I=NE+1,NDIM
+            S = 0
+            DO J = 1,NE
+                  S = S + Mie*TANH( G * U(J) )
+            END DO
+            DO J = NE+1,NDIM
+                  IF (J /= I) THEN
+                        S = S + Mii*TANH( G * U(J) )
+                  END IF
+            END DO
+            S = S / SQRT(REAL(NDIM))
+            F(I) = -U(I) + S 
+      END DO
+
+      ! DO I=1,NDIM
+      !       S = 0
+      !       DO J = 1,NDIM
+      !             S = S + H(I,J)*TANH( G * U(J) )
+      !       END DO
+      !       S = S / SQRT(REAL(NDIM))
+      !       F(I) = -U(I) + S 
+      ! END DO
+
+END SUBROUTINE FUNC
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+
+SUBROUTINE STPNT(NDIM,U,PAR,T)
+!     ---------- -----
+
+! Input arguments :
+!      NDIM   :   Dimension of the ODE system 
+
+! Values to be returned :
+!      U      :   A starting solution vector
+!      PAR    :   The corresponding equation-parameter values
+
+      IMPLICIT NONE
+      INTEGER NDIM
+      INTEGER LOFFSET, ROFFSET
+      INTEGER I
+      DOUBLE PRECISION U(NDIM), PAR(*), T
+      DOUBLE PRECISION D, K, W, PHI
+      DOUBLE PRECISION G, Mee, Mei, Mie, Mii
+
+! Initialize the equation parameters
+      G = 5
+      Mee = 0.25
+      Mei = 0.25
+      Mie = -1
+      Mii = -1
+
+      ! initialize to 0
+      DO I = 1,NDIM
+          U(I) = 0
+      END DO
+      U(NDIM) = 0.1204
+      U(NDIM-1) = 0.1204
+      U(NDIM-2) = -0.1204
+      U(NDIM-3) = -0.1204
+      
+      PAR(1) = G
+      PAR(2) = Mee
+      PAR(3) = Mei
+      PAR(4) = Mie
+      PAR(5) = Mii
+
+END SUBROUTINE STPNT
+
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
+! The following subroutines are not used here,
+! but they must be supplied as dummy routines
+
+      SUBROUTINE BCND 
+      END SUBROUTINE BCND
+
+      SUBROUTINE ICND 
+      END SUBROUTINE ICND
+
+      SUBROUTINE FOPT 
+      END SUBROUTINE FOPT
+
+      SUBROUTINE PVLS
+      END SUBROUTINE PVLS
+!----------------------------------------------------------------------
+!----------------------------------------------------------------------
